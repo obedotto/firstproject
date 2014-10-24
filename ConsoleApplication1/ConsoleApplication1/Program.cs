25c13ed51c46db023b564bbd8834826a97ba16eb
obedotto <obedotto@gmail.com>
2014-10-24 16:14:52 +0530

    after test

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Source control...");
+            Console.WriteLine("Welcome to Source control..."); 
         }
     }
 }
